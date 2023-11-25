0f629f94fbe7b0081f50bbc086c2c594228bede9
TMInstaller <ajvldmlqjqclr@naver.com>
2023-11-25 20:50:12 +0900

    MainProject 구현

--- src/app/(info)/projects/page.tsx
+++ src/app/(info)/projects/page.tsx
@@ -1,11 +1,10 @@
+import MainProjectInfo from '@/components/info/MainProjectInfo'
 import withLayout from '@/hoc/withLayout'
 
 const Projects: React.FC = () => {
   return (
     <div className='flex flex-col items-center'>
-      <div className='max-w-content w-full mt-4'>
-        <div className='border border-white'>여기는 Projects Page입니다</div>
-      </div>
+      <MainProjectInfo />
     </div>
   )
 }
---
+++ src/components/info/MainProjectInfo.tsx
@@ -0,0 +1,65 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
+const MainProjectInfo = () => {
+  return (
+    <div className='grid grid-cols-12 max-w-content w-full h-[30rem] mt-4 bg-background-light p-4 rounded-xl gap-4'>
+      <div className='col-start-1 col-end-7 relative bg-background-light'>
+        <Image
+          src='/RememberPlus.png'
+          alt=''
+          fill
+          style={{ objectFit: 'cover', overflow: 'hidden' }}
+        />
+      </div>
+      <div
+        className={`col-start-7 col-end-13 p-4 bg-background-light flex flex-col justify-center gap-2`}
+      >
+        <div className='flex flex-row gap-2 mb-4'>
+          <div className='text-5xl font-bold'>Project</div>
+          <div className='text-2xl font-bold'>Remember Plus</div>
+        </div>
+        <div className='flex flex-row gap-4 hover:border border-white'>
+          <div className='text-xl font-bold'>Info</div>
+          <div className='text-xl'>명함 관계 시각화 서비스</div>
+        </div>
+        <div className='flex flex-row gap-4 hover:border border-white'>
+          <div className='text-xl font-bold'>Period</div>
+          <div className='text-xl'>2023.07 ~ 2023.11</div>
+        </div>
+        <div className='flex flex-row gap-4 hover:border border-white'>
+          <div className='text-xl font-bold'>Position</div>
+          <div className='text-xl'>Frontend, DevOps</div>
+        </div>
+        <div className='flex flex-row gap-4 hover:border border-white'>
+          <div className='text-xl font-bold'>Stacks</div>
+          <div className='text-xl'>React.js, TailwindCSS, Three.js, D3.js</div>
+        </div>
+        <div className='flex flex-col gap-4'>
+          <div className='text-xl font-bold'>Visit Articles</div>
+          <div className='w-full h-32 bg-background-box rounded-lg grid grid-cols-3 py-2 hover:scale-105'>
+            <div className='relative col-start-1 col-end-2 hover:scale-105'>
+              <Link href={`https://github.com/SV-Summer-BootCamp-Team-F`}>
+                <Image src='/github.png' alt='' fill style={{ objectFit: 'contain' }} />
+              </Link>
+            </div>
+            <div className='relative col-start-2 col-end-3 hover:scale-105'>
+              <Link
+                href={`https://medium.com/@tminstaller/siliconvalley-winter-bootcamp-remember-plus-korean-803311e5fa9c`}
+              >
+                <Image src='/medium.png' alt='' fill style={{ objectFit: 'contain' }} />
+              </Link>
+            </div>
+            <div className='relative col-start-3 col-end-4 hover:scale-105'>
+              <Link href={`https://time-map-installer.tistory.com/250`}>
+                <Image src='/tistory.png' alt='' fill style={{ objectFit: 'contain' }} />
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default MainProjectInfo
