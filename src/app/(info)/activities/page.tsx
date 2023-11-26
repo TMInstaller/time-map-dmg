71b5eedca628101e1b66c64e78b476cd7baca341
TMInstaller <ajvldmlqjqclr@naver.com>
2023-11-26 20:09:46 +0900

    데이터 외부 처리

--- src/app/(info)/activities/page.tsx
+++ src/app/(info)/activities/page.tsx
@@ -5,7 +5,16 @@ const Activities: React.FC = () => {
   return (
     <div className='flex flex-col items-center'>
       <div className='text-5xl font-bold mt-4'>Activities</div>
-      <ActivityInfo />
+      <ActivityInfo
+        title='REMEMBER PLUS'
+        activityType='Team Project'
+        description='당신의 명함관계를 밤하늘의 별자리로'
+        where='2023 Silicon Valley Summer Bootcamp'
+        period='2023.07 ~ 2023.11'
+        result='종합 2위'
+        moreInfoURL='https://time-map-installer.tistory.com/250'
+        imageUrl='/RememberPlus.png'
+      />
     </div>
   )
 }
--- src/components/info/ActivityInfo.tsx
+++ src/components/info/ActivityInfo.tsx
@@ -1,36 +1,56 @@
 import Image from 'next/image'
 import Link from 'next/link'
 
-const ActivityInfo = () => {
+type ActivityInfoProps = {
+  title: string
+  activityType: string
+  description: string
+  where: string
+  period: string
+  result: string
+  moreInfoURL: string
+  imageUrl: string
+}
+
+const ActivityInfo: React.FC<ActivityInfoProps> = ({
+  title,
+  activityType,
+  description,
+  where,
+  period,
+  result,
+  moreInfoURL,
+  imageUrl,
+}) => {
   return (
     <div className='grid grid-cols-12 max-w-content w-full h-80 mt-4 animate-slide-in bg-background-light p-4 rounded-xl'>
       <div className='col-start-1 col-end-6 relative bg-background-light'>
-        <Image src='/RememberPlus.png' alt='' fill style={{ objectFit: 'contain' }} />
+        <Image src={imageUrl} alt={title} fill style={{ objectFit: 'contain' }} />
       </div>
       <div
         className={`col-start-6 col-end-13 p-4 bg-background-light flex flex-col justify-center gap-2`}
       >
         <div className='flex flex-row gap-2'>
-          <div className='text-4xl font-bold'>REMEMBER PLUS</div>
-          <div className='text-xl font-bold'>Team Project</div>
+          <div className='text-4xl font-bold'>{title}</div>
+          <div className='text-xl font-bold'>{activityType}</div>
         </div>
         <div className='flex flex-row gap-4'>
-          <div className='text-xl'>당신의 명함관계를 밤하늘의 별자리로</div>
+          <div className='text-xl'>{description}</div>
         </div>
         <div className='flex flex-row gap-4'>
           <div className='text-xl font-bold'>Where</div>
-          <div className='text-xl'>2023 SilliconValley Summer Bootcamp</div>
+          <div className='text-xl'>{where}</div>
         </div>
         <div className='flex flex-row gap-4'>
           <div className='text-xl font-bold'>Period</div>
-          <div className='text-xl'>2023.07 ~ 2023.11</div>
+          <div className='text-xl'>{period}</div>
         </div>
         <div className='flex flex-row gap-4'>
           <div className='text-xl font-bold'>Result</div>
-          <div className='text-xl'>종합 2위</div>
+          <div className='text-xl'>{result}</div>
         </div>
         <div className='text-xl font-bold text-red-500 hover:underline'>
-          <Link href='https://time-map-installer.tistory.com/250' target='_blank'>
+          <Link href={moreInfoURL} target='_blank'>
             More Information(Visit Tistory)
           </Link>
         </div>
