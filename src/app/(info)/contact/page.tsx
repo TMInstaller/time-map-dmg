75e2fd56527679985863a8ba29166b7657db3203
TMInstaller <ajvldmlqjqclr@naver.com>
2023-11-26 16:40:15 +0900

    공통 css 모듈화

--- src/app/(info)/contact/page.tsx
+++ src/app/(info)/contact/page.tsx
@@ -1,43 +1,45 @@
 import withLayout from '@/hoc/withLayout'
 
 const Contact: React.FC = () => {
+  const commonStyle = 'col-start-5 col-end-8 row-start-2 row-end-3 py-8 px-12'
+
   return (
     <div className='flex flex-col items-center'>
       <div className='max-w-content w-full mt-6'>
         <div className='grid grid-cols-11 grid-rows-3 h-[84vh] gap-2'>
-          <div className='col-start-5 col-end-8 row-start-2 row-end-3 py-8 px-12 animate-orbit1'>
+          <div className={`${commonStyle} animate-orbit1`}>
             <div className='h-full bg-background-box grid justify-center items-center rounded-xl'>
               1
             </div>
           </div>
-          <div className='col-start-5 col-end-8 row-start-2 row-end-3 py-8 px-12 animate-orbit2'>
+          <div className={`${commonStyle} animate-orbit2`}>
             <div className='h-full bg-background-box grid justify-center items-center rounded-xl'>
               2
             </div>
           </div>
 
-          <div className='col-start-5 col-end-8 row-start-2 row-end-3 py-8 px-12 animate-orbit3'>
+          <div className={`${commonStyle} animate-orbit3`}>
             <div className='h-full bg-background-box grid justify-center items-center rounded-xl'>
               3
             </div>
           </div>
-          <div className='col-start-5 col-end-8 row-start-2 row-end-3 py-8 px-12'>
+          <div className={`${commonStyle}`}>
             <div className='h-full bg-background-box grid justify-center items-center rounded-xl hover:animate-pulse'>
               center
             </div>
           </div>
-          <div className='col-start-5 col-end-8 row-start-2 row-end-3 py-8 px-12 animate-orbit4'>
+          <div className={`${commonStyle} animate-orbit4`}>
             <div className='h-full bg-background-box grid justify-center items-center rounded-xl'>
               4
             </div>
           </div>
 
-          <div className='col-start-5 col-end-8 row-start-2 row-end-3 py-8 px-12 animate-orbit5'>
+          <div className={`${commonStyle} animate-orbit5`}>
             <div className='h-full bg-background-box grid justify-center items-center rounded-xl'>
               5
             </div>
           </div>
-          <div className='col-start-5 col-end-8 row-start-2 row-end-3 py-8 px-12 animate-orbit6'>
+          <div className={`${commonStyle} animate-orbit6`}>
             <div className='h-full bg-background-box grid justify-center items-center rounded-xl'>
               6
             </div>
