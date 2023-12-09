9cadae3dae4ed507acfd9d0777b80be92bf9301c
TMInstaller <ajvldmlqjqclr@naver.com>
2023-12-09 18:23:15 +0900

    색상 미지정 오류 수정

--- src/components/global/Header.tsx
+++ src/components/global/Header.tsx
@@ -6,7 +6,7 @@ const Header = () => {
   return (
     <div className='flex items-center justify-center w-screen h-20 bg-background-light'>
       <div className='flex justify-between w-full max-w-content'>
-        <div className='font-serif text-3xl font-bold'>
+        <div className='font-serif text-3xl font-bold text-white'>
           <Link href='/'>TimeMap.dmg</Link>
         </div>
         <div className='flex row-auto gap-20'>
