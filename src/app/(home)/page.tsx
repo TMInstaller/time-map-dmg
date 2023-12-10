adf6ef03f2bc444e9f36b73a1aa87d2f21a744f0
TMInstaller <ajvldmlqjqclr@naver.com>
2023-12-10 15:36:34 +0900

    remove unnecessary code

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -4,16 +4,6 @@ import Link from 'next/link'
 import React, { useState } from 'react'
 
 export default function Home() {
-  const [tooltip, setTooltip] = useState('')
-
-  const handleMouseEnter = (category: string) => {
-    setTooltip(`More about ${category}`)
-  }
-
-  const handleMouseLeave = () => {
-    setTooltip('')
-  }
-
   const container: string = 'grid grid-cols-2 gap-4 w-screen h-screen p-10 text-center z-1'
   const serviceName: string =
     'col-span-2 text-6xl font-bold flex justify-center items-center hover:animate-pulse text-white'
@@ -30,16 +20,9 @@ export default function Home() {
         {categories.map((category, index) => (
           <Link key={index} href={`/${category.replace(/\s+/g, '').toLowerCase()}`} passHref>
             <div
-              className={categoryClass}
-              onMouseEnter={() => handleMouseEnter(category)}
-              onMouseLeave={handleMouseLeave}
+              className={`${categoryClass} hover:animate-pulse hover:bg-black hover:bg-opacity-40 hover:rounded-xl`}
             >
               {category}
-              {tooltip === `More about ${category}` && (
-                <div className='absolute w-full p-2 mt-2 bg-gray-700 border text-white'>
-                  {tooltip}
-                </div>
-              )}
             </div>
           </Link>
         ))}
