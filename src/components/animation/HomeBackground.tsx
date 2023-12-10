dc4c7a1eb27cfefe5fa73945b1d073c02c13a91a
TMInstaller <ajvldmlqjqclr@naver.com>
2023-12-10 15:31:35 +0900

    click animation 적용 완료

--- src/components/animation/HomeBackground.tsx
+++ src/components/animation/HomeBackground.tsx
@@ -1,11 +1,21 @@
 import './HomeBackground.style.css'
 
 const HomeBackground = () => {
+  const animateButton = (e: any) => {
+    e.preventDefault()
+    const target = e.target
+    target.classList.add('animateClick')
+
+    setTimeout(() => {
+      target.classList.remove('animateClick')
+    }, 750) // 0.75초 후에 클래스 제거
+  }
+
   return (
     <div className='w-screen h-screen fixed z-0'>
       <ul className='circles'>
         {[...Array(10)].map((_, i) => (
-          <li key={i}></li>
+          <li key={i} onClick={animateButton}></li>
         ))}
       </ul>
     </div>
