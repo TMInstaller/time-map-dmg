496247b20eca8ee8e94e78104d46fe1a1794aacf
TMInstaller <ajvldmlqjqclr@naver.com>
2023-12-11 19:23:00 +0900

    반응형 디자인 적용

--- src/components/info/ActivityInfo.tsx
+++ src/components/info/ActivityInfo.tsx
@@ -26,16 +26,15 @@ const ActivityInfo: React.FC<ActivityInfoProps> = ({
   const router = useRouter()
 
   const container = {
-    common:
-      'grid w-[92%] grid-cols-12 p-4 mt-4 max-w-content animate-slide-in bg-background-light rounded-xl',
-    hover: 'hover:animate-pulse hover:cursoe-pointer',
-    web: 'md:h-80',
-    mobile: 'h-24',
+    common: 'p-4 mt-4 bg-background-light rounded-xl animate-slide-in',
+    hover: 'hover:animate-pulse hover:cursor-pointer',
+    web: 'grid grid-cols-12 w-[92%] max-w-content',
+    mobile: 'grid grid-cols-1',
   }
 
   return (
     <div
-      className={`${container.common} ${container.hover} ${container.web} ${container.mobile}`}
+      className={`${container.common} ${container.hover} ${container.mobile} md:${container.web}`}
       onClick={() => router.push(moreInfoURL)}
     >
       <div className='relative col-start-1 col-end-6 bg-background-light'>
@@ -43,33 +42,33 @@ const ActivityInfo: React.FC<ActivityInfoProps> = ({
           src={imageURL}
           alt={title}
           fill
-          sizes='(min-width: 512px) 600px, 600px'
-          style={{ overflow: 'hidden', borderRadius: '0.75rem' }}
+          sizes='(max-width: 768px) 100vw, (max-width: 1024px) 50vw, 33vw'
+          style={{ objectFit: 'cover', borderRadius: '0.75rem' }}
         />
       </div>
       <div
         className={`col-start-6 col-end-13 p-4 bg-background-light flex flex-col justify-center gap-2`}
       >
-        <div className='flex flex-row gap-2'>
-          <div className='text-2xl font-bold md:text-4xl'>{title}</div>
+        <div className='flex flex-col gap-2 md:flex-row'>
+          <div className='text-xl font-bold md:text-4xl'>{title}</div>
           <div className='text-sm font-bold md:text-xl'>{activityType}</div>
         </div>
         <div className='flex flex-row gap-4'>
           <div className='text-sm md:text-xl'>{description}</div>
         </div>
-        <div className='flex flex-row gap-4'>
+        <div className='flex-row hidden gap-4 md:flex'>
           <div className='text-sm font-bold md:text-xl'>Where</div>
           <div className='text-sm md:text-xl'>{where}</div>
         </div>
-        <div className='flex flex-row gap-4'>
+        <div className='flex-row hidden gap-4 md:flex'>
           <div className='text-sm font-bold md:text-xl'>Period</div>
           <div className='text-sm md:text-xl'>{period}</div>
         </div>
-        <div className='flex flex-row gap-4'>
+        <div className='flex-row hidden gap-4 md:flex'>
           <div className='text-sm font-bold md:text-xl'>Result</div>
           <div className='text-sm md:text-xl'>{result}</div>
         </div>
-        <div className='text-sm font-bold text-red-500 md:text-xl hover:underline'>
+        <div className='hidden text-sm font-bold text-red-500 md:block md:text-xl hover:underline'>
           <Link href={moreInfoURL} target='_blank'>
             More Information
           </Link>
