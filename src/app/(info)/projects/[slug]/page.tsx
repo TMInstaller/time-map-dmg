c0a8f8ffb69fe0d29ed111d00af906923681f624
TMInstaller <bjdy3000@gmail.com>
2024-01-17 23:57:53 +0900

    마크다운 서식 추가

--- src/app/(info)/projects/[slug]/page.tsx
+++ src/app/(info)/projects/[slug]/page.tsx
@@ -31,17 +31,28 @@ function ProjectPage({ params }: { params: { slug: string } }) {
   return (
     <div className='mt-8 flex justify-center items-center'>
       {/* 화면 전체 높이에 걸쳐 중앙 정렬 */}
-      <article className='w-full md:w-1/2 flex flex-col'>
+      <article className='w-full md:w-2/3 flex flex-col px-4 md:px-0'>
         {/* 전체 너비에서 반으로 줄임 */}
-        <h1 className='text-4xl font-bold'>{project.title}</h1>
+        <h1 className='text-4xl font-bold text-center'>{project.title}</h1>
         <ReactMarkdown
           components={{
             // 예를 들어 h1 태그에 대한 커스텀 렌더링
             h1: ({ node, ...props }) => <h1 className='text-3xl font-bold my-4' {...props} />,
+            h2: ({ node, ...props }) => <h2 className='text-2xl font-bold my-4' {...props} />,
+            h3: ({ node, ...props }) => <h3 className='text-xl font-bold my-4' {...props} />,
             p: ({ node, ...props }) => <p className='my-2 text-base' {...props} />,
             a: ({ node, ...props }) => <a className='text-blue-600 hover:underline' {...props} />,
             ul: ({ node, ...props }) => <ul className='list-disc list-inside my-4' {...props} />,
             li: ({ node, ...props }) => <li className='ml-4' {...props} />,
+            blockquote: ({ node, ...props }) => (
+              <blockquote
+                className='border-l-4 border-blue-600 pl-4 py-2 italic bg-blue-50 text-black'
+                {...props}
+              />
+            ),
+            code: ({ node, ...props }) => (
+              <code className='bg-gray-600 rounded p-[2px] font-mono text-sm' {...props} />
+            ),
           }}
         >
           {content}
